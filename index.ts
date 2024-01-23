e22db654675413516bb2b84785ef0c43f6f2d803
Corey Burns <coreymichaelburns@tutanota.com>
2024-01-23 15:32:45 -0500

    added restart function, error checking

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import type { Request, Response } from 'express';
 import express from 'express';
+import process from 'process';
 import { createClient } from 'redis';
 
 async function main() {
@@ -15,12 +16,16 @@ async function main() {
 
   app.get('/', async (req: Request, res: Response) => {
     const views = await client.get('views');
-    res.send(`Views = ${views} (asdfdsf?)`);
+    res.send(`Views = ${views}`);
 
     views
       ? // set the views key, but parse it as int first then add one
         await client.set('views', parseInt(views) + 1)
       : await client.disconnect();
+  });
+
+  app.get('/foo', (req: Request, res: Response) => {
+    process.exit(0);
   });
 
   const port = 3000;
